79df66df1829924a58e98c4c86bded2d15caae23
David Lin Wang <143329630+DementDavid@users.noreply.github.com>
2023-09-06 07:57:40 +0200

    Hamburger menu functions

---
+++ hamburger menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileMenuButton = document.querySelector('.mobile-menu-button');
+  const topnav = document.querySelector('.topnav');
+
+  mobileMenuButton.addEventListener('click', function () {
+    topnav.classList.toggle('open'); // Ajoute ou supprime la classe "open" pour afficher ou masquer le menu
+  });
+});
