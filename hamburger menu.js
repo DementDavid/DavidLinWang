eebfceb5a137ab24b0b0eedbbe37ddabbaae957b
David Lin Wang <143329630+DementDavid@users.noreply.github.com>
2023-09-08 14:42:33 +0200

    Update hamburger menu.js

--- hamburger menu.js
+++ hamburger menu.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const mobileMenuButton = document.querySelector('.mobile-menu-button');
   const mobilemenu = document.querySelector('.mobile-menu');
-
+  function checkClickOutside(event) {
+    if (!mobileMenuButton.contains(event.target) && !mobilemenu.contains(event.target)) {
+      mobilemenu.classList.remove('open');
+    }
+  } 
   mobileMenuButton.addEventListener('click', function () {
     mobilemenu.classList.toggle('open');
   });
+    document.addEventListener('click', checkClickOutside);
 });
