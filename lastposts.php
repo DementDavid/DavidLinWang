692f1f7c703228e4a19656b5f02981b728da2272
David Lin Wang <143329630+DementDavid@users.noreply.github.com>
2023-09-09 21:44:05 +0200

    Update lastposts.php

--- lastposts.php
+++ lastposts.php
@@ -1,17 +1,29 @@
-fetch('php2.json') 
-				.then(response => response.json())
-				.then(articles => {
-					const recentArticlesContainer = document.querySelector('.recent-articles');
-					const maxRecentArticles = 5; 
+fetch('php2.json')
+  .then(response => response.json())
+  .then(articles => {
+    const recentArticlesContainer = document.querySelector('.recent-articles');
+    const maxRecentArticles = 5;
 
-					for (let i = 0; i < Math.min(articles.length, maxRecentArticles); i++) {
-						const articleDiv = document.createElement('div');
-						articleDiv.className = 'recent-article';
-						const a = document.createElement('a');
-						a.href = articles[i].lien;
-						a.textContent = articles[i].titre;
-						articleDiv.appendChild(a);
-						recentArticlesContainer.appendChild(articleDiv);
-					}
-				})
-				.catch(error => console.error('JSON error :', error));
+    for (let i = 0; i < Math.min(articles.length, maxRecentArticles); i++) {
+      const articleDiv = document.createElement('div');
+      articleDiv.className = 'recent-article';
+
+      const articleLink = document.createElement('a');
+      articleLink.href = articles[i].lien;
+      articleLink.textContent = articles[i].titre;
+      articleDiv.appendChild(articleLink);
+
+      const articleContent = document.createElement('p');
+      articleContent.className = 'article-content';
+      articleContent.textContent = articles[i].content;
+      articleDiv.appendChild(articleContent);
+
+      const articleDate = document.createElement('p');
+      articleDate.className = 'article-date';
+      articleDate.textContent = articles[i].date;
+      articleDiv.appendChild(articleDate);
+
+      recentArticlesContainer.appendChild(articleDiv);
+    }
+  })
+  .catch(error => console.error('JSON error:', error));
