1c2c1dbcc2da4d9854f50b4e9c9fee9afed2b2d3
David Lin Wang <143329630+DementDavid@users.noreply.github.com>
2023-12-06 21:11:22 +0100

    Create enciclopedyportal.js

---
+++ enciclopedyportal.js
@@ -0,0 +1,4 @@
+
+    document.getElementById('accessibility-button').addEventListener('click', function() {
+      window.location.href = "https://dementdavid.github.io/DavidLinWang.github.io/enciclopedy.html";
+    });
