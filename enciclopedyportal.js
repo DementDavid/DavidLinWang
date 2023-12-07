723a86ffac786b45033d67eb2821f407576b8aab
David Lin Wang <143329630+DementDavid@users.noreply.github.com>
2023-12-07 21:00:44 +0100

    Update enciclopedyportal.js

--- enciclopedyportal.js
+++ enciclopedyportal.js
@@ -1,5 +1,5 @@
 
-    const eportal=document.getElementById('accessibility-button')
+    const eportal=document.getElementById('enciclopedy-button')
     eportal.addEventListener('click', function() {
       window.location.href = "https://dementdavid.github.io/DavidLinWang.github.io/enciclopedy.html";
     });
