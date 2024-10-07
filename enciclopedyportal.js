5f9afbd317a4bbb5afdac248c3189a5509e983e9
David Lin Wang <143329630+DementDavid@users.noreply.github.com>
2024-10-07 21:45:56 +0200

    .

--- enciclopedyportal.js
+++ enciclopedyportal.js
@@ -1,5 +1,5 @@
 
     const eportal=document.getElementById('enciclopedy-button')
     eportal.addEventListener('click', function() {
-      window.location.href = "https://dementdavid.github.io/DavidLinWang.github.io/enciclopedy.html";
+      window.location.href = "https://dementdavid.github.io/DavidLinWang.github.io/ZPost/enciclopedy.html";
     });
